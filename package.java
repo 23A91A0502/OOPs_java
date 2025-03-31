02180f6eacedf07269d96c77d7be1e44d8320fdb
Madhumitha Akula <23A91A0502@aec.edu.in>
2025-03-31 13:22:14 +0530

    Update to package.java

---
+++ package.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Packa{
+    public static void main(String[] args){
+         Scanner read=new Scanner(System.in);
+         String username;
+         username=read.nextLine();
+         System.out.println("Username: " + username);
+    }
+}
